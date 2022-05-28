62dcf12bd8711d269659f9066f9e056bef8cfcc6
Mohammad Razi ul Haq <razi.ulhaq@creditbook.pk>
2022-05-28 20:18:34 +0500

    main users table

--- data-model/models/staging/base/base_workspaces.sql
+++ data-model/models/staging/base/base_workspaces.sql
@@ -3,4 +3,4 @@ owner_user_id as user_id,
 workspace_id,
 country_id,
 created_at
- FROM `toggl-analytics.sources.workspaces` 
+ FROM {{ source('sources', 'workspaces') }}
--- data-model/models/staging/stg_user_monthly_charges.sql
+++ data-model/models/staging/stg_user_monthly_charges.sql
@@ -1,8 +1,8 @@
 with data as 
 (
   Select * from {{ref('inter_subscription_charges')}}
-)
-
+),
+temp as(
 select d.subscription_period_id,
 d.workspace_id,
 d.is_trial,
@@ -12,10 +12,9 @@ d.finished_on,
 d.sub_user_count as user_count,
 d.pricing_plan_id,
 d.amount_usd,
-us.owner_user_id as user_id
+us.user_id as user_id
 from data d
-left join {{ source('sources', 'workspaces') }} us
+left join {{ref('base_workspaces')}} us
 on d.workspace_id=us.workspace_id
-where d.sub_user_count = d.charges_user_count  
-and d.amount_usd is not null
-
+)
+select * from temp where user_id is not null
